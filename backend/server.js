f68eb0bbb6fb3c58a57541202123bf03f8b07921
Anshikais <anshikaparmar30@gmail.com>
2026-05-08 21:36:45 +0530

    Updated environment variables

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/snapchef')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected to snapchef database'))
   .catch(err => console.error('MongoDB connection error:', err));
 
